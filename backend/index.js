c1a5771b194f596881652b18015a86e0740f739a
vedantyerekar <vedantyerekar1605@gmail.com>
2026-07-21 13:51:18 +0530

    Added Backend

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,17 @@ MongoClient.connect(
     uri,
     {
         maxPoolSize: 50,
-        wtimeoutMS:2500,
+        wtimeoutMS: 2500,
     }
 )
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-})
-.then(async client =>{
-    await ReviewsDAO.injectDB(client)
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
     })
-})
+    .then(async client => {
+        await ReviewsDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
 
